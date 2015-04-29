db22052fad7ededb0cf6160225a7f6c8b4cd1755
unknown <joojen@gmail.com>
2015-04-29 15:59:28 +0800

    嵌入式google自定义搜索框

---
+++ typegrid11/searchform.php
@@ -0,0 +1,8 @@
+<form method="get" action="http://www.google.com.hk/search" target="_blank">
+<input type="text" name="q" size="26" />
+<input type="hidden" name="ie" value="UTF-8" />
+<input type="hidden" name="oe" value="UTF-8" />
+<input type="hidden" name="hl" value="zh-CN" />
+<input type="hidden" name="domains" value="www.keege.com" />
+<input type="hidden" name="sitesearch" value="www.keege.com" />
+</form>
