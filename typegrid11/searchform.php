eecb08ac9ac928687f60c3e40ff93c8e49f2797b
unknown <joojen@gmail.com>
2015-04-29 14:39:06 +0800

    修改搜索框为百度搜索

---
+++ typegrid11/searchform.php
@@ -0,0 +1,9 @@
+
+<form method="get" action="http://www.baidu.com/baidu">
+<input name="word" size="26" type="text">
+<input name="ie" value="utf-8" type="hidden"> 
+<input name="tn" value="bds" type="hidden">
+<input name="cl" value="3" type="hidden">
+<input name="ct" value="2097152" type="hidden">
+<input name="si" value="www.keege.com" type="hidden">
+</form>
